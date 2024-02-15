6ad54a40c960773d76678651dc73d8c5fd732dac
Mário S. Mira <marioasmira@gmai.com>
2024-02-15 15:35:14 +0100

    Added function to combine logs created with a parallel loop

--- R/droso_utils.R
+++ R/droso_utils.R
@@ -258,3 +258,26 @@ find_density_peaks <- function(numbers,
          decreasing = decreasing)
   return(second_peak_x)
 }
+
+#' Combine log files into a single file and delete old files
+#'
+#' @param log_directory directory where to find the log files
+#'
+#' @return Nothing
+#' @export
+combine_logs <- function(log_directory) {
+  # Get the names of the log files
+  log_files <- list.files(path = log_directory, pattern = "log_")
+  # Combine the log files
+  log <- unlist(lapply(paste0(log_directory, log_files), readLines))
+  # Write the combined log to a file
+  writeLines(log,
+             con = paste0(
+               log_directory,
+               as.character(Sys.Date()),
+               "_combined_log.txt"
+             ))
+
+  # Delete the separate log files
+  file.remove(paste0(log_directory, log_files))
+}
