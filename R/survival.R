bc2a29ea7d9fc4a4dbc5c4ec28ccf2e1dd6e3ae2
Mário S. Mira <marioasmira@gmai.com>
2024-02-25 18:08:45 +0100

    survival file

---
+++ R/survival.R
@@ -0,0 +1,25 @@
+# survival.R
+
+#' Fly survival
+#' @description
+#' Calculates the survival of a fly by creating a square-shaped function with
+#' two logistic curves and a modified normal distribution pdf.
+#'
+#' @param temperature The current temperature for which to test survival
+#' @param experience The phenotype (midpoint) of the fly to be tested.
+#' @returns The probability of survival of the fly.
+#' @export
+#' @importFrom rcspline logis
+survival <- function(temperature, experience) {
+  logis(temperature,
+        x_0 = experience - 9,
+        k = 1.0,
+        max = 0.8) +
+    logis(temperature,
+          x_0 = experience + 9,
+          k = -1.0,
+          max = 0.8) -
+    0.8 +
+    exp(-((temperature - experience) ^ 2) /
+          (2.0 * (9 * 2) ^ 2)) / (9 * 2 * sqrt(2 * pi))
+}
