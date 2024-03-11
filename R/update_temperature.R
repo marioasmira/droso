c4a6c94e21ef599a15b9385767610786107771b4
Mário S. Mira <marioasmira@gmai.com>
2024-03-11 13:05:06 +0100

    update_temperature has variable random error

--- R/update_temperature.R
+++ R/update_temperature.R
@@ -1,4 +1,3 @@
-
 #' Update temperature
 #' @description
 #' Calculates the temperature of a specific day, taking into account the
@@ -10,6 +9,7 @@
 #' @param autocorr The environmental autocorrelation between days.
 #' @param base_env The base value around which the sine wave will change.
 #' @param amplitude The amplitude of the sine wave.
+#' @param rand_env Amount of error to add to the environment.
 #' @param period The period of the sine wave.
 #' @returns Returns the value of temperature of the specified day.
 #' @export
@@ -20,6 +20,7 @@ update_temperature <-
            autocorr,
            base_env,
            amplitude,
+           rand_env,
            period) {
     stopifnot(autocorr >= 0 && autocorr <= 1)
     previous_temperature <- temperature
@@ -27,7 +28,6 @@ update_temperature <-
       autocorr *
         previous_temperature +
         (1 - autocorr) *
-        sine_wave(base_env, amplitude, day, period)
-    ) + rnorm(n = 1, mean = 0, sd = 1)
-    )
+          sine_wave(base_env, amplitude, day, period)
+    ) + rnorm(n = 1, mean = 0, sd = rand_env))
   }
