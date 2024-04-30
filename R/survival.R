2b1dc6ed3b9f14d41e9f6a684438ba7fedca756c
Mário S. Mira <marioasmira@gmai.com>
2024-04-30 11:21:30 +0200

    using the squarelike funciton for survival

--- R/survival.R
+++ R/survival.R
@@ -3,7 +3,9 @@
 #' Fly survival
 #' @description
 #' Calculates the survival of a fly by creating a square-shaped function with
-#' two logistic curves and a modified normal distribution pdf.
+#' two logistic curves and a modified normal distribution pdf. This is a wrapper
+#' for the squarelike function with the survival specific values already filled
+#' in.
 #'
 #' @param temperature The current temperature for which to test survival
 #' @param experience The phenotype (midpoint) of the fly to be tested.
@@ -11,15 +13,12 @@
 #' @export
 #' @importFrom rcspline logis
 survival <- function(temperature, experience) {
-  logis(temperature,
-    x_0 = experience - 9,
-    k = 1.0,
+  squarelike(
+    x = temperature,
+    l_inflection = experience - 9,
+    l_slope = 1.0,
+    r_inflection = experience + 9,
+    r_slope = -1.0,
     max = 0.8
-  ) +
-    logis(temperature,
-      x_0 = experience + 9,
-      k = -1.0,
-      max = 0.8
-    ) -
-    0.8
+  )
 }
